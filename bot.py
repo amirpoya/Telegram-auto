4001a299f90e95fc15e6ec4f1f759073e9090214
amirpoya <114870920+amirpoya@users.noreply.github.com>
2025-08-12 20:46:39 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
+from flask import Flask, jsonify
+import os, socket, datetime as dt
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return f"OK from {socket.gethostname()} at {dt.datetime.utcnow().isoformat()}Z"
+
+@app.get("/health")
+def health():
+    return jsonify(status="healthy", port=os.environ.get("PORT"))
+
+if __name__ == "__main__":
+    # برای اجرای لوکال
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
